82465a82fdf02aff82e99adc8eb54da8a478c127
Sparsh Bamrara <sparsh.bamrara@finbox.in>
2024-12-15 11:01:42 +0530

    feat: added authentication

--- controllers/authentication/user.py
+++ controllers/authentication/user.py
@@ -6,7 +6,6 @@ from dotenv import load_dotenv
 from fastapi import Request
 from utils.logger import logger
 
-# from connections.mongo.authentication import authenticate
 load_dotenv()
 
 def authenticated_user(request: Request) -> bool:
